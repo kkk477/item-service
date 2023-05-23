8f8f02c4d61edc4648cde9299fb06a1865126ecc
HoWoong <dlghdnd@gmail.com>
2023-05-23 15:46:33 +0900

    스프링 MVC 1편 - 백엔드 웹 개발 핵심 기술. 끝.

--- src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -5,9 +5,8 @@ import hello.itemservice.domain.item.ItemRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -23,6 +22,84 @@ public class BasicItemController {
         List<Item> items = itemRepository.findAll();
         model.addAttribute("items", items);
         return "basic/items";
+    }
+
+    @GetMapping("/{itemId}")
+    public String item(@PathVariable long itemId, Model model) {
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item", item);
+        return "basic/item";
+    }
+
+    @GetMapping("/add")
+    public String addForm() {
+        return "basic/addForm";
+    }
+
+//    @PostMapping("/add")
+//    public String addItemV1(@RequestParam String itemName,
+//                       @RequestParam int price,
+//                       @RequestParam Integer quantity,
+//                       Model model) {
+//
+//        Item item = new Item();
+//        item.setItemName(itemName);
+//        item.setPrice(price);
+//        item.setQuantity(quantity);
+//
+//        itemRepository.save(item);
+//
+//        model.addAttribute("item", item);
+//
+//        return "basic/item";
+//    }
+
+//    @PostMapping("/add")
+//    public String addItemV2(@ModelAttribute("item") Item item) {
+//
+//        itemRepository.save(item);
+////      model.addAttribute("item", item); // @ModelAttribute("item") 덕분에 파라미터의 Model도 지울 수 있음
+//
+//        return "basic/item";
+//    }
+
+//    @PostMapping("/add")
+//    public String addItemV3(@ModelAttribute Item item) {
+//        itemRepository.save(item); // 기본으로 Item -> item으로 바꿔서 ModelAttribute에 넣음
+//        return "basic/item";
+//    }
+
+//    @PostMapping("/add")
+//    public String addItemV4(Item item) {
+//        itemRepository.save(item); // 내가 만든 class는 알아서 ModelAttribute에 적용해줌
+//        return "basic/item";
+//    }
+
+//    @PostMapping("/add")
+//    public String addItemV5(Item item) {
+//        itemRepository.save(item); //
+//        return "redirect:/basic/items/" + item.getId();
+//    }
+
+    @PostMapping("/add")
+    public String addItemV6(Item item, RedirectAttributes redirectAttributes) {
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:/basic/items/{itemId}";
+    }
+
+    @GetMapping("/{itemId}/edit")
+    public String editForm(@PathVariable Long itemId, Model model) {
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item", item);
+        return "basic/editForm";
+    }
+
+    @PostMapping("/{itemId}/edit")
+    public String edit(@PathVariable Long itemId, @ModelAttribute Item item) {
+        itemRepository.update(itemId, item);
+        return "redirect:/basic/items/{itemId}";
     }
 
     @PostMapping
